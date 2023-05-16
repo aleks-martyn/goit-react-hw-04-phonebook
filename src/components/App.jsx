a79d28a2d7bcefe508d626312707109bfa438f48
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-16 23:29:38 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,5 +6,34 @@ import { ContactList } from './ContactList';
 import { Container } from './App.styled';
 
 export const App = () => {
-  return <Container></Container>;
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const formSubmitHandler = formData => {
+    const { name } = formData;
+
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === name.toLowerCase()
+      )
+    )
+      alert(`${name} is already in contacts`);
+
+    const contact = { id: shortid.generate(), ...formData };
+
+    setContacts(prev => [contact, ...prev]);
+  };
+
+  const changeFilter = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={changeFilter} />
+    </Container>
+  );
 };
