183d65d61038f4d6f7ac0996fddea8a1a6b02169
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-05-16 21:56:29 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,16 +3,26 @@ import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import { Form, FormLabel, Input } from './ContactForm.styled';
 
-export const ContactForm = () => {
+export const ContactForm = ({ onSubmit }) => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
 
   const nameInputId = shortid.generate();
   const numberInputId = shortid.generate();
 
-  handleSubmit = event => {
+  const handleChange = event => {
+    const { name, value } = event.currentTarget;
+    name === 'name' ? setName(value) : setNumber(value);
+  };
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const handleSubmit = event => {
     event.preventDefault();
-    props.onSubmit(state);
+    onSubmit({ name, number });
     reset();
   };
 
@@ -21,9 +31,30 @@ export const ContactForm = () => {
       <FormLabel htmlFor={nameInputId}>
         <span>Name</span>
       </FormLabel>
+      <Input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        value={name}
+        onChange={handleChange}
+        id={nameInputId}
+      />
+
       <FormLabel htmlFor={numberInputId}>
         <span>Number</span>
       </FormLabel>
+      <Input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        value={number}
+        onChange={handleChange}
+        id={numberInputId}
+      />
 
       <button type="submit">Add contact</button>
     </Form>
